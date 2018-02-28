26b8526d046476192fa70bec1cf8515a6b017db3
wynn <wynn.bailey@gmail.co>
2018-02-28 11:46:48 -0800

    Initial Commit

---
+++ git_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git_test
+//
+//  Created by Wynn Bailey on 2/28/18.
+//  Copyright © 2018 Wynn Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
